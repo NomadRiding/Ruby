659e5ac31dae704e2961c9262cdca4ff35201430
leopolicastro <lpolicastro84@gmail.com>
2018-11-22 11:24:00 -0500

    CodeCademy Ruby first commit

---
+++ codecademy.rb
@@ -0,0 +1,49 @@
+# Data Types: Numbers Strings, Booleans
+# my_num = 25    # Add your code here!
+
+# my_boolean = true    # And here!
+
+# my_string =  "Ruby"   # Also here.
+
+# puts "My number is equal to #{my_num}"
+# puts  "I have set my_boolean to be equal to #{my_boolean}"
+# puts  "I have set my_string to be equal to #{my_string}"
+#
+#
+# puts "Fifteen plus 12 is equal to #{15 + 12}"
+# puts "3 to the 3rd power is #{3**3}"
+# puts "The Remainder of 6 divided by 4 is #{6%4}"
+#
+#
+# puts "Never Stop Learning"
+# print "Wyncode Academy"
+# puts "Never Stop Learning"
+# print "Wyncode Academy"
+#
+#
+# puts "The length of this sentence includinng all spaces and characters is".length
+# puts "My name in reverse would be #{"leo policastro".reverse} "
+# puts "Leo Policastro".upcase
+# puts 'Leo Policastro'.downcase
+#
+=begin
+everything in here is a comment?
+everything in here is a comment?
+everything in here is a comment?
+everything in here is a comment?
+everything in here is a comment?
+=end
+# name = "Leo Policastro"
+# puts name
+#
+#
+sum = 13 +379
+product = 923 * 15
+quotient = 13209 /17
+
+puts "The sum of 13 and 379 is equal to #{sum}"
+puts "923 multipied by 15 is equal to #{product}"
+puts "13209 divided by 17 is equal to #{quotient}" 
+puts name = "Leo Policastro".downcase.reverse.upcase
+
+
