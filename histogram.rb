f7667fc2f6c72be55527e56c91f812576463e50c
leopolicastro <lpolicastro84@gmail.com>
2018-11-23 07:54:21 -0500

    OCD

--- histogram.rb
+++ histogram.rb
@@ -4,6 +4,6 @@ text = gets.chomp
 words = text.split(" ")
 frequencies = Hash.new(0)
 words.each { |word| frequencies[word] += 1 }
-frequencies = frequencies.sort_by { |a, b| b }
+frequencies = frequencies.sort_by { |k, v| v }
 frequencies.reverse!
 frequencies.each { |word, frequency| puts word + " " + frequency.to_s }
---
+++ methods.rb
@@ -0,0 +1,13 @@
+def puts_1_to_10
+  (1..10).each { |i| puts i }
+end
+
+puts_1_to_10
+#
+#
+def greeting
+  puts "What's Up Doc?"
+end
+greeting
+
+
