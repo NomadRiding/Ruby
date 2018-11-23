6254d12320d5d3ab61b69e4a10ba0146bd7b5388
leopolicastro <lpolicastro84@gmail.com>
2018-11-23 06:52:27 -0500

    Back it all up

---
+++ arrays.rb
@@ -0,0 +1,8 @@
+demo_array = [100, 200, 300, 400, 500]
+
+print demo_array[2] # Add your code here!
+##
+##
+multi_d_array = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
+
+multi_d_array.each { |x| puts "#{x}\n" }
---
+++ hashes.rb
@@ -0,0 +1,72 @@
+# my_hash = {"name" => "Eric",
+#            "age" => 26,
+#            "hungry?" => true}
+
+# puts my_hash["name"]
+# puts my_hash["age"]
+# puts my_hash["hungry?"]
+
+
+=begin
+pets = Hash.new
+
+pets["Juice"] = "cat"
+
+puts "Our new pet is a #{pets['Juice']}" 
+
+
+friends = ["Milhouse", "Ralph", "Nelson", "Otto"]
+
+family = {"Homer" => "dad",
+          "Marge" => "mom",
+          "Lisa" => "sister",
+          "Maggie" => "sister",
+          "Abe" => "grandpa",
+          "Santa's Little Helper" => "dog"}
+
+friends.each { |x| puts "#{x}" }
+family.each { |x, y| puts "#{x}: #{y}" }
+=end
+
+# numbers = [1, 2, 3, 4, 5]
+# numbers.each { |element| puts element }
+
+# ## is the same as 
+# languages = ["HTML", "CSS", "JavaScript", "Python", "Ruby"]
+
+# languages.each { |x| puts x }
+
+
+# s = [["ham", "swiss"], ["turkey", "cheddar"], ["roast beef", "gruyere"]]
+
+# s.each { |sub_array|
+#   sub_array.each { |x|
+#     puts x
+#   }
+# }
+
+# secret_identities = {
+#   "The Batman" => "Bruce Wayne",
+#   "Superman" => "Clark Kent",
+#   "Wonder Woman" => "Diana Prince",
+#   "Freakazoid" => "Dexter Douglas",
+# }
+
+# secret_identities.each do |x, y|
+#   puts "#{x}: #{y}"
+# end
+
+
+##
+# lunch_order = {
+#   "Ryan" => "wonton soup",
+#   "Eric" => "hamburger",
+#   "Jimmy" => "sandwich",
+#   "Sasha" => "salad",
+#   "Cole" => "taco",
+# }
+
+# lunch_order.each {
+#   |x, y|
+#   puts y
+# }
